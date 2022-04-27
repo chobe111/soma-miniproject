0a9efcce007bad8ea999912b828ec8adbe29055a
chobe111 <chobe1@naver.com>
2022-04-27 16:17:52 +0900

    First commit

---
+++ main.js
@@ -0,0 +1,7 @@
+const formContainer = document.querySelector(".form-container");
+
+const handleSubmit = (event) => {
+  console.log("data sumit!!");
+};
+
+formContainer.addEventListener("submit", handleSubmit);
