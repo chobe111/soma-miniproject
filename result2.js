97ac048446910cb9f30cf640857c71832215dd63
chobe111 <chobe1@naver.com>
2022-04-30 18:34:25 +0900

    Implement Table head

---
+++ result2.js
@@ -0,0 +1,57 @@
+const testData = [
+  {
+    name: "asdf",
+    mbti: "ENFP",
+    color: "red",
+  },
+  {
+    name: "asdf",
+    mbti: "ENFP",
+    color: "red",
+  },
+  {
+    name: "asdf",
+    mbti: "ENFP",
+    color: "red",
+  },
+  {
+    name: "asdf",
+    mbti: "ENFP",
+    color: "red",
+  },
+  {
+    name: "asdf",
+    mbti: "ENFP",
+    color: "red",
+  },
+  {
+    name: "asdf",
+    mbti: "ENFP",
+    color: "red",
+  },
+  {
+    name: "asdf",
+    mbti: "ENFP",
+    color: "red",
+  },
+  {
+    name: "asdf",
+    mbti: "ENFP",
+    color: "red",
+  },
+  {
+    name: "asdf",
+    mbti: "ENFP",
+    color: "red",
+  },
+  {
+    name: "asdf",
+    mbti: "ENFP",
+    color: "red",
+  },
+  {
+    name: "asdf",
+    mbti: "ENFP",
+    color: "red",
+  },
+];
